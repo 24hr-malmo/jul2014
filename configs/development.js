8e8a89dc9097c99f86130d61b7d920f35dc4dd4d
camilo tapia <camilo.tapia@gmail.com>
2014-12-03 10:47:44 +0100

    removed configs

--- configs/development.js
+++ configs/development.js
@@ -1,24 +1,12 @@
 exports.server = {
-    port: 10014
-};
-
-exports.realtime = {
-    port: 10015
+    port: 8014
 };
 
 exports.db = {
-    database: 'goals_courses',
-    username: 'goals_courses_user',
-    password: 'goals_courses_user_77'
+    path: './data/db'
 };
 
 exports.log = {
     level: 'warn'
-};
-
-exports.newrelic = {
-    enabled: false,
-    name: 'Goals Course Service - Development',
-    key: '85f52b2f89abf9fc5cf3591a1273e3caf4bdfa4a'
 };
 
